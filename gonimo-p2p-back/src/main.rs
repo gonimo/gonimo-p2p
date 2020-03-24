b17c71526da9b1311045df0279b1ad107383e037
Robert Klotzner <robert.klotzner@gmx.at>
2020-03-24 19:20:23 +0100

    Loopification

--- gonimo-p2p-back/src/main.rs
+++ gonimo-p2p-back/src/main.rs
@@ -28,37 +28,42 @@ async fn main() -> std::io::Result<()> {
     let keypair = ed25519::Keypair::generate();
     let my_peer_id = PeerId::from_public_key(PublicKey::Ed25519(keypair.public()));
     let service = MdnsService::new().expect("Another mdns service already running?");
-    let _future_to_poll = async {
-		let (mut service, packet) = service.next().await;
-
-		match packet {
-			MdnsPacket::Query(query) => {
-				println!("Query from {:?}", query.remote_addr());
-				let resp = build_query_response(
-					query.query_id(),
-					my_peer_id.clone(),
-					vec![].into_iter(),
-					Duration::from_secs(120),
-				).unwrap();
-				service.enqueue_response(resp);
-			}
-			MdnsPacket::Response(response) => {
-				for peer in response.discovered_peers() {
-					println!("Discovered peer {:?}", peer.id());
-					for addr in peer.addresses() {
-						println!("Address = {:?}", addr);
-					}
-				}
-			}
-			MdnsPacket::ServiceDiscovery(disc) => {
-				let resp = build_service_discovery_response(
-					disc.query_id(),
-					Duration::from_secs(120),
-				);
-				service.enqueue_response(resp);
-			}
-		}
-	};
-	_future_to_poll.await;
+    async {
+        println!("Before asking next");
+        let mut next = service.next().await;
+        println!("Before loop");
+		loop {
+            let (mut service, packet) = next;
+            println!("iteration");
+            match packet {
+                MdnsPacket::Query(query) => {
+                    println!("Query from {:?}", query.remote_addr());
+                    let resp = build_query_response(
+                        query.query_id(),
+                        my_peer_id.clone(),
+                        vec![].into_iter(),
+                        Duration::from_secs(120),
+                    ).unwrap();
+                    service.enqueue_response(resp);
+                }
+                MdnsPacket::Response(response) => {
+                    for peer in response.discovered_peers() {
+                        println!("Discovered peer {:?}", peer.id());
+                        for addr in peer.addresses() {
+                            println!("Address = {:?}", addr);
+                        }
+                    }
+                }
+                MdnsPacket::ServiceDiscovery(disc) => {
+                    let resp = build_service_discovery_response(
+                        disc.query_id(),
+                        Duration::from_secs(120),
+                    );
+                    service.enqueue_response(resp);
+                }
+            }
+            next = service.next().await;
+        }
+	}.await;
 	Ok(())
 }
